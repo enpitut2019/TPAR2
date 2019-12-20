13b008e7acacc3d7a95527f8c5c80233a6333503
shumaikunkun <s1711520@gmail.com>
2019-12-20 16:47:20 +0900

    first commit

---
+++ Assets/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float advanceBullet = -0.3f;
+
+    void Update()
+    {
+        transform.Translate(0, 0, this.advanceBullet);
+        if (transform.position.z < -10.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerDamage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage : MonoBehaviour
+{
+    int playerHP;
+    
+
+    private void Start()
+    {
+        playerHP = 3;        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    //　コライダのIsTriggerのチェックを入れ物理的な衝突をしない（突き抜ける）場合
+    void OnTriggerEnter(Collider col) {
+        Debug.Log("hit something");
+		if(col.tag == "Enemy") {
+            playerHP -= 1;
+            if (playerHP < 1)
+            {
+                Destroy(this.gameObject);  //Erase Player
+
+            }
+            Debug.Log(playerHP);
+
+        }
+	}
+
+}
